f6fe97b949c27df0439d73cc0fc27bd76b26e065
Xizhe <rayinnz1999@gmail.com>
2022-10-02 16:47:12 +1100

    web dynamic css in vue from Xiaoshan

---
+++ vue_dynamic_css/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div :style="declareBgColor" class="bg">test</div>
+
+    <input v-model="bgColor" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bgColor: "blue",
+    };
+  },
+  computed: {
+    declareBgColor() {
+      return `--bgColor: ${this.bgColor};`;
+    },
+  },
+};
+</script>
+
+<style>
+.bg {
+  /* css variable */
+
+  background-color: var(--bgColor);
+}
+.bg2 {
+}
+</style>
