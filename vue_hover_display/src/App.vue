98b7ea5c87a275abb346d1684e94b5ae332ba43f
Raymond <rayinnz1999@gmail.com>
2022-10-05 00:28:46 +1100

    vue 鼠标悬停显示样式

---
+++ vue_hover_display/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <button @mouseover="show = !show" @mouseleave="show = !show">Toggle Slide + Fade</button>
+    <Transition name="slide-fade">
+      <p v-if="show">hello</p>
+    </Transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      // active: false,
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+
+ .slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+} 
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
