6cee798735417e8bb833a75b10d9992fff8b7e07
Xizhe <rayinnz1999@gmail.com>
2023-01-17 11:24:47 +1100

    Switch (react)

---
+++ Switch/switch/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>123123</p>
+      <input type="checkbox" id="toggle" class="offscreen" />
+      <label for="toggle" class="switch"></label>
+    </div>
+  );
+}
+
+export default App;
